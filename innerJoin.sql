69ab12b821b2afc3310f9d2e14f420f6f8fbbca6
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-25 18:26:17 +0700

    inner JOIN

---
+++ innerJoin.sql
@@ -0,0 +1,16 @@
+﻿USE HowKteam
+GO
+
+-- inner join -> kieu cu -> có thể sau này không còn được hỗ trợ
+-- mỗi inner join đều cần điều kiện
+SELECT * FROM dbo.GIAOVIEN, dbo.BOMON
+WHERE dbo.BOMON.MABM = dbo.GIAOVIEN.MABM
+
+
+-- inner join kiểu mới
+SELECT * FROM dbo.GIAOVIEN INNER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+-- viết tắt
+SELECT * FROM dbo.KHOA JOIN dbo.BOMON
+ON BOMON.MAKHOA = KHOA.MAKHOA
