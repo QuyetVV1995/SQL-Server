b71d9ac8e90807ffe601612cc8209d43a6887f78
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-26 13:12:40 +0700

    group by having

---
+++ Groupby_Having.sql
@@ -0,0 +1,8 @@
+﻿USE HowKteam
+GO
+
+-- xuất ra số lượng giáo viên trong từng bộ môn mà số giáo viên > 2
+-- having -> như where của select nhưng dành cho group by
+SELECT dbo.BOMON.MABM, COUNT(*) FROM dbo.GIAOVIEN, dbo.BOMON
+WHERE dbo.BOMON.MABM = dbo.GIAOVIEN.MABM
+GROUP BY dbo.BOMON.MABM HAVING COUNT(*) > 2
