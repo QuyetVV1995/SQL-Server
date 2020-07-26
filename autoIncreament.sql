3b969b797117a06b8b66b18ee33fb490d98d9fcd
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-26 13:23:39 +0700

    autoIncreament

---
+++ autoIncreament.sql
@@ -0,0 +1,60 @@
+﻿USE HowKteam
+GO
+
+CREATE TABLE autoIncreament(
+	ID INT PRIMARY KEY IDENTITY,	-- IDENTITY tự động tăng, phải là số, mặc định tăng 1
+	firstName NVARCHAR(100)
+)
+GO
+
+
+INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+	INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+)
+INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+	INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+	INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+	INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+	INSERT dbo.autoIncreament
+(
+    firstName
+)
+VALUES
+(N'' -- firstName - nvarchar(100)
+    )
+GO
