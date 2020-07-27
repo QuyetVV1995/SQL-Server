04d2add7e16757ab62a944fcf1377bbcdebe39ef
QuyetVV95 <quyeta2ubqn@gmail.com>
2020-07-27 16:49:02 +0700

    kieu du lieu tu dinh nghia

---
+++ kieudulieutudinhnghia.sql
@@ -0,0 +1,14 @@
+﻿USE HowKteam
+GO
+
+-- kiểu dữ liệu tự định nghĩa
+-- EXEC sp_addtype ' tên kiểu dữ liệu ', ' kiểu dữ liệu thực tế', 'Not null' (có hay không đều được)
+-- kiểu dữ liệu mới nằm trong Programmability/Types/User-Define Data Types
+EXEC sp_addtype 'NName', 'nvarchar(100)', 'Not null'
+
+CREATE TABLE testType1(
+	firstName NName,
+	address NVARCHAR(10)
+)
+GO
+
